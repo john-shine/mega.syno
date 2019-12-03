1be922143119fa1aa943de9736a2c91c5eba9c9e
john-shine <mr.john.shine@gmail.com>
2019-12-03 01:24:38 -0800

    remove tar owner info

--- synology-package/pkgCreate.sh
+++ synology-package/pkgCreate.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-tar -czf package.tgz ./package/*
+tar -czf package.tgz --owner=0 --group=0 ./package/*
 mkdir -p build/
 mkdir -p conf/
 rm -f build/MEGAsync.spk
-tar -cvf build/MEGAsync.spk scripts/* INFO PACKAGE_ICON.PNG PACKAGE_ICON_256.PNG LICENSE package.tgz conf/
+tar -cvf build/MEGAsync.spk --owner=0 --group=0 scripts/* INFO PACKAGE_ICON.PNG PACKAGE_ICON_256.PNG LICENSE package.tgz conf/
 rm -f package.tgz
